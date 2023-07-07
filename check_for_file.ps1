3b0e0d2efa55b2792af2d0d054c356c4e0a28d2e
sylvesteryiadom <32847798+sylvesteryiadom@users.noreply.github.com>
2023-07-07 17:34:55 -0500

    Create check_for_file.ps1

---
+++ check_for_file.ps1
@@ -0,0 +1,47 @@
+# param (
+#     [Parameter(Mandatory=$true)]
+#     [string]$folderPath
+# )
+
+# # Check if the folder exists
+# if (-not (Test-Path -Path $folderPath -PathType Container)) {
+#     Write-Host "Input folder not found."
+#     exit 1 # Failed exit code
+# }
+
+# # Get all text files in the folder
+# $textFiles = Get-ChildItem -Path $folderPath -Filter "*.txt" -File
+
+# # Check if any text files were found
+# if ($textFiles.Count -gt 0) {
+#     Write-Host "Text file(s) found in input folder."
+#     exit 0 # Success exit code
+# } else {
+#     Write-Host "No text files found in input folder."
+#     exit 1 # Failed exit code
+# }
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$folderPath
+)
+
+# Check if the folder exists
+if (-not (Test-Path -Path $folderPath -PathType Container)) {
+    Write-Host "Input folder not found."
+    exit 1 # Failed exit code
+}
+
+# Get all items (files and folders) in the folder
+$items = Get-ChildItem -Path $folderPath
+
+# Check if any files were found (excluding folders)
+$fileCount = ($items | Where-Object { -not $_.PSIsContainer }).Count
+
+# Check if any files (excluding folders) were found
+if ($fileCount -gt 0) {
+    Write-Host "File(s) found in input folder."
+    exit 0 # Success exit code
+} else {
+    Write-Host "No files found in input folder."
+    exit 1 # Failed exit code
+}
