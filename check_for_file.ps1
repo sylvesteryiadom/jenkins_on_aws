ee59167d2af6eab5a2e7ae25683b7914747fa802
sylvesteryiadom <32847798+sylvesteryiadom@users.noreply.github.com>
2023-08-18 12:55:18 -0500

    Update check_for_file.ps1

--- check_for_file.ps1
+++ check_for_file.ps1
@@ -52,3 +52,66 @@ if ($fileCount -gt 0) {
 
 
 # Get-ChildItem -Path "C:\path\to\directory" -Directory | ForEach-Object { $_.FullName + ": " + "{0:N2}" -f ((Get-ChildItem -Path $_.FullName -Recurse | Measure-Object -Property Length -Sum).Sum / 1MB) + " MB" }
+
+# NEW SCRIPT
+
+param (
+    [string]$folderNamesDir,
+    [string]$jobFilesDir
+)
+
+# Read folder names from the input text file
+$folderNames = Get-Content -Path $folderNamesDir
+
+foreach ($folderName in $folderNames) {
+    $folderPath = Join-Path -Path $jobFilesDir -ChildPath $folderName
+
+    if (Test-Path -Path $folderPath -PathType Container) {
+        Write-Host "Folder '$folderName' already exists."
+
+        $subfolders = @("input", "output", "history")
+
+        foreach ($subfolder in $subfolders) {
+            $subfolderPath = Join-Path -Path $folderPath -ChildPath $subfolder
+
+            if (-not (Test-Path -Path $subfolderPath -PathType Container)) {
+                New-Item -Path $subfolderPath -ItemType Directory
+                Write-Host "Subfolder '$subfolder' created in '$folderName'."
+            } else {
+                Write-Host "Subfolder '$subfolder' already exists in '$folderName'."
+            }
+        }
+    } else {
+        New-Item -Path $folderPath -ItemType Directory
+        Write-Host "Folder '$folderName' created."
+
+        $subfolders = @("input", "output", "history")
+
+        foreach ($subfolder in $subfolders) {
+            $subfolderPath = Join-Path -Path $folderPath -ChildPath $subfolder
+            New-Item -Path $subfolderPath -ItemType Directory
+            Write-Host "Subfolder '$subfolder' created in '$folderName'."
+        }
+    }
+}
+
+Write-Host "Folder creation process completed."
+
+# ./mkdir_folders.ps1 -folderNamesDir "/Users/sylvesteryiadom/Desktop/folder_names.txt" -jobFilesDir "/Users/sylvesteryiadom/Desktop/input"
+
+# GET FOLDER NAMES SCRIPT
+
+# Path to the directory containing the folders
+$directoryPath = "~/Desktop/allfolder"
+
+# Path to the text file where folder names will be written
+$outputFilePath = "~/Desktop/folder_names.txt"
+
+# Get a list of folder names in the directory
+$folderNames = Get-ChildItem -Path $directoryPath -Directory | Select-Object -ExpandProperty Name
+
+# Write the folder names to the output text file
+$folderNames | Out-File -FilePath $outputFilePath
+
+Write-Host "Folder names have been copied to $outputFilePath"
+
